cb224aa1280e92bcb20e6c84f64e28d86567b949
usamayasin708 <usama.zamstudios@gmail.com>
2022-03-15 16:02:20 +0500

    setup react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './myComponents/Header'
+import { Footer } from './myComponents/Footer';
+import { Todo } from './myComponents/Todo';
+import { Todos } from './myComponents/Todos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Footer />
+      <Todos />
+      <Todo />
+    </>
   );
 }
 
